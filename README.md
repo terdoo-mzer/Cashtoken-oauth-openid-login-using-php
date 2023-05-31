6a285ac172159554febe9f2b52fc30bd3e8ecf53
Mzer Emmanuel Terdoo <mzerterdoo6@gmail.com>
2023-05-31 20:44:19 +0100

    fix styling and add README

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# cashtoken-oauth-php
+# Cashtoken oauth/openid login using php
+
+## Installation
+
+```bash
+1. Install PHP (preferably version 8.x) on your computer
+
+2. Clone the repository (as zip)
+
+3. Extract the zip file to your own projects folder
+
+4. Open the terminal
+
+5. Navigate to project directory where number 3 is located
+
+6. Run the command: php -S localhost:3000 to run the PHP local server
+
+7. Run localhost:3000 in a browser
+```
+
--- dashboard.php
+++ dashboard.php
@@ -1,40 +1,44 @@
-<?php 
+<?php
 session_start();
-
 if (isset($_SESSION['active']) && (time() - $_SESSION['active'] > 600)) {
-  header('location: http://localhost:3000/openid.php?logout');
-  die();
+    header('location: http://localhost:3000/auth.php?logout');
+    die();
 }
 $_SESSION['active'] = time();
 
 if (!isset($_SESSION['user_data'])) {
-  header('location: http://localhost:3000');
-  die();
-} 
+    header('location: http://localhost:3000');
+    die();
+}
 
 $data = json_decode($_SESSION['user_data']);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
     <link rel="stylesheet" href="./assets/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
+
 <body>
     <!-- <p><?= $str ?></p> -->
-    <a class="btn" href="http://localhost:3000/auth.php?logout">Logout</a>
-    <div class="container">
-        <?php
-            print_r($data);
-            echo "<pre>";
-        ?>
+    <a class="btn logout_btn" href="http://localhost:3000/auth.php?logout">Logout</a>
 
-        <?php if(isset($_SESSION)) {?>
-            <?= print_r($_SESSION)?>
-        <?php } ?>
+
+    <div class="container">
+        <p class="title"><span class="bold">User ID: </span><span><?= $data->user_id ?></span></p>
+        <p class="title"><span class="bold">Username: </span><span><?= $data->username ?></span></p>
+        <p class="title"><span class="bold">Display Name: </span><span><?= $data->display_name ?></span></p>
+        <p class="title"><span class="bold">First Name: </span><span><?= $data->first_name ?></span></p>
+        <p class="title"><span class="bold">Last Name: </span><span><?= $data->last_name ?></span></p>
+        <p class="title"><span class="bold">Email: </span><span><?= $data->email ?></span></p>
     </div>
+
 </body>
+
 </html>
