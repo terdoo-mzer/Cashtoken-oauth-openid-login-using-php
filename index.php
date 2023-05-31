8ae41fb78a6ed93cbd2de79425ac08d16cad98f4
Mzer Emmanuel Terdoo <mzerterdoo6@gmail.com>
2023-05-31 12:00:43 +0100

    add files

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    $str = "Hello Terdoo";
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="./assets/style.css">
+</head>
+<body>
+    <p><?= $str ?></p>
+    <a class="btn" href="">Login with Cashtoken</a>
+</body>
+</html>
